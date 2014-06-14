a42bb4442b2de45efe05c3d5ecf448f4886e5f19
Anatole Tresch <atsticks@gmail.com>
2014-06-14 13:38:38 +0200

    Added and tested example TCK setup.

---
+++ src/main/java/RunTCK.java
@@ -0,0 +1,11 @@
+import org.javamoney.tck.TCKRunner;
+
+/**
+ * Created by Anatole on 14.06.2014.
+ */
+public class RunTCK{
+
+    public static void main(String[] args){
+        TCKRunner.main(args);
+    }
+}
