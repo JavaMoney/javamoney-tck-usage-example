fd0d3dc5f7b45c1877b4c386ad0f2262439d1791
atsticks <mw34rkf>
2014-08-22 00:34:25 +0200

    -fixed test setup.

--- src/main/java/MonetaTCKSetup.java
+++ src/main/java/MonetaTCKSetup.java
@@ -16,6 +16,7 @@
 
 import org.javamoney.moneta.FastMoney;
 import org.javamoney.moneta.function.MonetaryFunctions;
+import org.javamoney.moneta.function.MonetaryUtil;
 import org.javamoney.tck.JSR354TestConfiguration;
 
 import javax.money.MonetaryOperator;
@@ -74,14 +75,14 @@ public final class MonetaTCKSetup implements JSR354TestConfiguration {
     @Override
     public Collection<MonetaryOperator> getMonetaryOperators4Test() {
         List<MonetaryOperator> ops = new ArrayList<>();
-        ops.add(MonetaryFunctions.majorPart());
-        ops.add(MonetaryFunctions.minorPart());
-        ops.add(MonetaryFunctions.percent(BigDecimal.ONE));
-        ops.add(MonetaryFunctions.percent(3.5));
-        ops.add(MonetaryFunctions.permil(10.3));
-        ops.add(MonetaryFunctions.permil(BigDecimal.ONE));
-        ops.add(MonetaryFunctions.permil(10.5, MathContext.DECIMAL32));
-        ops.add(MonetaryFunctions.reciprocal());
+        ops.add(MonetaryUtil.majorPart());
+        ops.add(MonetaryUtil.minorPart());
+        ops.add(MonetaryUtil.percent(BigDecimal.ONE));
+        ops.add(MonetaryUtil.percent(3.5));
+        ops.add(MonetaryUtil.permil(10.3));
+        ops.add(MonetaryUtil.permil(BigDecimal.ONE));
+        ops.add(MonetaryUtil.permil(10.5, MathContext.DECIMAL32));
+        ops.add(MonetaryUtil.reciprocal());
         ops.add(MonetaryRoundings.getDefaultRounding());
         ops.add(MonetaryConversions.getConversion("EUR"));
         return ops;
