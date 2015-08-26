6ac5188c4500b6ab9d6da1a46a09b1f6c488f54c
Werner Keil <werner.keil@gmx.net>
2015-08-26 23:01:02 +0200

    21: Adjust TCK Examples Fixed typo in package Task-Url: http://github.com/JavaMoney/javamoney-examples/issues/issue/21

--- src/main/java/org/javamoney/test/tck/TCKTestSetup.java
+++ src/main/java/org/javamoney/test/tck/TCKTestSetup.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.javamoney.text.tck;
+package org.javamoney.test.tck;
 
 import org.javamoney.moneta.FastMoney;
 import org.javamoney.moneta.Money;
