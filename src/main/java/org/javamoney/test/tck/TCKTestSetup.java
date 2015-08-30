41d03d30f574f78d28ebdcbaa6e476f74a913f58
Werner Keil <werner.keil@gmx.net>
2015-08-30 21:37:00 +0200

    21: Adjust TCK Examples
    Task-Url: http://github.com/JavaMoney/javamoney-examples/issues/issue/21

--- src/main/java/org/javamoney/test/tck/TCKTestSetup.java
+++ src/main/java/org/javamoney/test/tck/TCKTestSetup.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012, 2014, Credit Suisse (Anatole Tresch), Werner Keil and others by the @author tag.
+ * Copyright (c) 2012, 2015, Credit Suisse (Anatole Tresch), Werner Keil and others by the @author tag.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -29,54 +29,56 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * JSR354TestConfiguration setup class. This is an example TCK setup class, that has to be written by
- * implementors to setup the JSR 354 TCK for running with their implementations.
+ * JSR354TestConfiguration setup class. This is an example TCK setup class, that
+ * has to be written by implementors to setup the JSR 354 TCK for running with
+ * their implementations.
  * <p>
  * Created by Anatole Tresch on 14.06.2014.
  */
 public final class TCKTestSetup implements JSR354TestConfiguration {
 
-    /**
-     * Provide the MonetaryAmount implementation classes to be tested for immutability and other
-     * implementation requirements.
-     *
-     * @return the monetary amount classes to be tested, not null and not empty.
-     */
-    @Override
-    public Collection<Class> getAmountClasses() {
-        return Arrays
-                .asList(new Class[]{Money.class, FastMoney.class});
-    }
+	/**
+	 * Provide the MonetaryAmount implementation classes to be tested for
+	 * immutability and other implementation requirements.
+	 *
+	 * @return the monetary amount classes to be tested, not null and not empty.
+	 */
+	@Override
+	public Collection<Class> getAmountClasses() {
+		return Arrays.asList(new Class[] { Money.class, FastMoney.class });
+	}
 
-    /**
-     * Provide the CurrencyUnit implementation classes to be tested for immutability and other
-     * implementation requirements.
-     *
-     * @return the currency unit classes to be tested, not null and not empty.
-     */
-    @Override
-    public Collection<Class> getCurrencyClasses() {
-        try {
-            return Arrays
-                    .asList(new Class[]{JDKCurrencyAdapter.class});
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Currency class not lodable: org.javamoney.moneta.internal.format.JDKCurrencyAdapter");
-        }
-    }
+	/**
+	 * Provide the CurrencyUnit implementation classes to be tested for
+	 * immutability and other implementation requirements.
+	 *
+	 * @return the currency unit classes to be tested, not null and not empty.
+	 */
+	@Override
+	public Collection<Class> getCurrencyClasses() {
+		try {
+			return Arrays.asList(new Class[] { JDKCurrencyAdapter.class });
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(
+					"Currency class not loadable: "
+							+ org.javamoney.moneta.internal.JDKCurrencyAdapter.class
+									.getName());
+		}
+	}
 
-    /**
-     * Provide the MonetaryOperator implementation classes to be tested for immutability and other
-     * implementation requirements.
-     *
-     * @return the monetary operator classes to be tested, not null and not empty.
-     */
-    @Override
-    public Collection<MonetaryOperator> getMonetaryOperators4Test() {
-        List<MonetaryOperator> ops = new ArrayList<>();
-        ops.add(Monetary.getDefaultRounding());
-        ops.add(MonetaryConversions.getConversion("EUR"));
-        return ops;
-    }
-
+	/**
+	 * Provide the MonetaryOperator implementation classes to be tested for
+	 * immutability and other implementation requirements.
+	 *
+	 * @return the monetary operator classes to be tested, not null and not
+	 *         empty.
+	 */
+	@Override
+	public Collection<MonetaryOperator> getMonetaryOperators4Test() {
+		List<MonetaryOperator> ops = new ArrayList<>();
+		ops.add(Monetary.getDefaultRounding());
+		ops.add(MonetaryConversions.getConversion("EUR"));
+		return ops;
+	}
 }
